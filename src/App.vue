1c27448134b2e1630d557b1d7586afbb54ace526
Niklas Schachl <niklas.schachl@htl-wels.at>
2023-01-11 11:36:22 +0100

    Move to branch master

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <Funding
+        :data="data"
+        :max="maxFunding"
+        :updateBewilligung="updateBewilligt"
+    />
+  </div>
+</template>
+
+<script>
+import Funding from './components/Funding.vue';
+import pvData from "./data.json"
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  components: {
+    Funding,
+  },
+  setup() {
+    const data = ref(pvData);/*ref([
+      {
+        name: 'Maier',
+        plz: '4020',
+        zaehlpunkt: '0000-5232-4199-1234-0983',
+        kwPeak: 14.4,
+        bewilligt: true,
+      },
+      {
+        name: 'Huber',
+        plz: '4620',
+        zaehlpunkt: '0000-1232-4189-1124-1983',
+        kwPeak: 10.0,
+        bewilligt: false,
+      },
+      {
+        name: 'Rager',
+        plz: '4400',
+        zaehlpunkt: '0450-9832-4109-1764-1999',
+        kwPeak: 19.0,
+        bewilligt: false,
+      },
+    ]);
+    */
+    const maxFunding = 10000000;
+
+    function updateBewilligt(index) {
+      console.log('[ OK ] Index: ' + index);
+      console.log('[ OK ] Data: ');
+      console.log(data.value);
+
+      data.value[index].bewilligt = !data.value[index].bewilligt;
+
+      console.log('[ CHANGED ] Data: ');
+      console.log(data.value);
+    }
+
+    return { data, maxFunding, updateBewilligt };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Funding.vue
@@ -0,0 +1,62 @@
+<template>
+  <h1>Bisherige Ansuchen (Max. Fördersumme: {{ max }}€)</h1>
+  <table>
+    <thead>
+    <tr><td><b>Name</b></td><td><b>PLZ.</b></td><td><b>Zählpunkt</b></td><td><b>kW peak</b></td><td><b>Bewilligt</b></td></tr>
+    </thead>
+    <tbody>
+    <tr v-for="(item, index) in data">
+      <td>{{ item.name }}</td><td>{{ item.plz }}</td><td>{{ item.zaehlpunkt }}</td><td>{{ item.kwPeak}}</td><td><input type="checkbox" :checked="item.bewilligt" @change="updateBewilligung(index)"></td>
+    </tr>
+    </tbody>
+    </table>
+</template>
+
+<script>
+export default
+{
+  props: {
+    data: { type: Array, required: true },
+    max: {type: Number, required: true},
+    updateBewilligung: {type: Function, required: true},
+  },
+};
+</script>
+
+<style scoped>
+*
+{
+  margin: auto;
+}
+thead
+{
+  background-color: #41B883;
+}
+thead tr
+{
+  background-color: #41B883;
+}
+table {
+  border-collapse: collapse;
+  margin-left: auto;
+  margin-right: auto;
+}
+td,
+th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+tr {
+  background-color: #f2f2f2;
+}
+tbody tr:hover {
+  background-color: #ddd;
+}
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #41b883;
+  color: white;
+}
+</style>
